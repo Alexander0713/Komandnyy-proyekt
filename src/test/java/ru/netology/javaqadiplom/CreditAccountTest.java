4c280378812b33a13f4cbf6c73c99077fd2a13f9
Alexandr <0231050@mail.ru>
2025-11-06 18:59:15 +0300

    New Bug report 1,2

--- src/test/java/ru/netology/javaqadiplom/CreditAccountTest.java
+++ src/test/java/ru/netology/javaqadiplom/CreditAccountTest.java
@@ -3,6 +3,8 @@ package ru.netology.javaqadiplom;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 public class CreditAccountTest {
 
     @Test
@@ -15,6 +17,33 @@ public class CreditAccountTest {
 
         account.add(3_000);
 
-        Assertions.assertEquals(3_000, account.getBalance());
+        assertEquals(3_000, account.getBalance());
     }
+    @Test
+
+    void shouldNotPayWhenAmountExceedsCreditLimit() {
+
+// Создаем счет с балансом 1000 и кредитным лимитом 5000
+        CreditAccount account = new CreditAccount(1000, 5000, 15);
+
+// Пытаемся оплатить 7000 (превышаем лимит)
+        boolean result = account.pay(7000);
+
+// Проверяем что операция не прошла
+        assertFalse(result);
+// Проверяем что баланс не изменился
+        assertEquals(1000, account.getBalance());
+    }
+    @Test
+    void shouldAddAmountToBalance() {
+        CreditAccount account = new CreditAccount(1000, 5000, 15);
+
+        boolean result = account.add(500);
+
+        assertTrue(result);
+        assertEquals(1500, account.getBalance());
+    }
+
+
 }
+
